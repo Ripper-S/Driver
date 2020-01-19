7be968665ff0b0bc7e9ed932f15e5b82f6da4a1f
Ripper_S <ripper-s@ripper-s.com>
2020-01-19 23:07:02 +0800

    change hello.c

--- mod/hello/hello.c
+++ mod/hello/hello.c
@@ -22,8 +22,8 @@ module_init(hello_init);
 module_exit(hello_exit);
 module_param(num, int, S_IRUGO);
 
-EXPORT_SYMBOL(add_integar);
+ EXPORT_SYMBOL(add_integar);	// 通过这个模块导出某些函数，使得这个函数在整个内核都可见，使得下一个模块可以直接调用上一个模块的这个导出函数。
 
-MODULE_AUTHOR("Jack He");
+MODULE_AUTHOR("Ripper-S");
 MODULE_DESCRIPTIOM("A simple hello world Module");
 MODULE_ALIAS("a simplest module");
